fc6d12f2e20c1e22699f6c2eb3e6826a9f0fa2b8
Ramyasnehitha <84697305+Ramyasnehitha@users.noreply.github.com>
2021-05-31 18:15:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,61 @@
+import './App.css';
+import React, { useState } from 'react'
+
+
+
+const Button = (props) => {
+  return (
+      <button onClick={props.event}>{props.text}</button>
+  )
+}
+const Statistic = (props) => {
+    return (
+        <p>{props.text} {props.value}</p>
+    )
+  }
+const Statistics = (props) => {
+    const all = props.good + props.bad + props.neutral
+    const average = (props.good + (props.bad*-1))/all
+      const positive = (props.good)/all *100
+      if(all===0){
+        return (
+            <p>No feedbacks given</p>
+        )
+    }
+      return(
+          <div>
+           <Statistic text="good" value={props.good} />
+                      <Statistic text="neutral" value={props.neutral} />
+                         <Statistic text="bad" value={props.bad} />
+                       <Statistic text="all" value={all} />
+                        <Statistic text="average" value={average} />
+                   <Statistic text="positive" value={positive} />
+
+          </div>
+
+      )
+}
+
+function App() {
+  const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    return (
+        <div>
+            <h1> give feedback </h1>
+            <div>
+                <Button event={()=> setGood(good+1)} text="good" />
+                <Button event={()=> setNeutral(neutral+1)} text="neutral" />
+                <Button event={()=> setBad(bad+1)} text="bad" />
+                                
+            </div>
+            <h2>statistics</h2>
+            <Statistics good={good} bad ={bad} neutral={neutral} />
+           
+        </div>
+    )
+}
+
+
+export default App;
