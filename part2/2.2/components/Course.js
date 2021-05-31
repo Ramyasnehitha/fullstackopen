2ceb50a446efcd14864139fffaf5de7b21084e33
Ramyasnehitha <84697305+Ramyasnehitha@users.noreply.github.com>
2021-05-31 16:40:09 +0530

    Update Course.js

--- part2/2.2/components/Course.js
+++ part2/2.2/components/Course.js
@@ -7,7 +7,7 @@ import React from 'react'
   }
   
   const Total = ({ course }) => {
-    const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises + course.parts[0].exercises
+    const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises + course.parts[3].exercises
     return (
         <strong>total of {sum} exercises</strong>
     )
